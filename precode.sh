eb30501571b7668e52c68efc34805babe25fb9ac
Abunasyr <abunasyr1@gmail.com>
2026-08-11 16:43:24 +0500

    feat: add creating folders

--- precode.sh
+++ precode.sh
@@ -7,26 +7,41 @@
 #   dir3
 #       dir4
 
+mkdir -p task/dir1 task/dir2 task/dir3/dir4
+
 # изменяем текущую директорию на task
 
+
+
 # создаём пустой файл task/dir2/empty
+
 
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
 # echo "$1, привет!"
 
+
+
+
+
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
+
 
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
 
+
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
+
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
 
+
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
 
+
 # определяем переменную окружения NAME со значением "Всем студентам"
+
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
